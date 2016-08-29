399f7335220ac4a9ed9eb7425d7b8c4748a6cdee
Thomas Wagner <tmw100@gmail.com>
2016-08-28 21:45:22 -0700

    Add responsive formatting for “content”

--- sites/all/modules/custom/idmod/js/idmdefault.js
+++ sites/all/modules/custom/idmod/js/idmdefault.js
@@ -13,9 +13,50 @@ Drupal.behaviors.idmdefault = {
 
     //  Patch to fix content div spacing. Problem is that the banner div is
     //  located below the content (thank you, Zen). The banner is place into
-    //  position with absolute - thus organic spacing is broken. 
+    //  position with absolute - thus organic spacing is broken.
+    
+    //  Test to see if tag exists...
     if ( $( ".full-banner" ).length ) {
-      $('#content').css('padding-top', '450px');
+      //Mobile
+      
+      //$('#content').css('padding-top', '40%');
+      
+      //  We need an initial setting.
+      
+      var winWidth = $( window ).width();
+      
+      if (winWidth <= 480){
+        $('#content').css('padding-top', '70%');
+      }
+      if (winWidth > 480){
+        $('#content').css('padding-top', '45%');
+      }
+      if (winWidth  >  959){
+        $('#content').css('padding-top', '40%');
+      }      
+      
+      //  Set parameters on window RESIZE  
+      $(window).resize(function(){
+        
+        if ($(window).width() <= 480){	
+          $('#content').css('padding-top', '70%');
+        }
+        
+        if ($(window).width() > 480){	
+          $('#content').css('padding-top', '45%');
+        }
+      
+        if ($(window).width() > 959){	
+          $('#content').css('padding-top', '40%');
+        }	        
+        
+      });
+      
+      //Tablet
+      
+      //Desktop
+      
+      
       
     } else {
       $('#content').css('padding-top', '0px');
